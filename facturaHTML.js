2b59e45f4e1ffc048c92e47272b19d36cf59612e
Carlos Vargas <carlosospino13@gmail.com>
2020-11-25 16:57:10 -0500

    Convertir HTML en PDF.

---
+++ facturaHTML.js
@@ -0,0 +1,41 @@
+const html = `
+    <h1>SAGPT</h1>
+
+    <h2>Cliente: Carlos Arturo Vargas</h2>
+    <h2>Documento: 1152457641</h2>
+
+    <h3 class="center">Productos comprados</h3>
+
+    <table>
+        <thead>
+         <tr>
+            <th>Categoría</th>
+            <th>Marca</th>
+            <th>Modelo</th>
+            <th>Precio unitario</th>
+         </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Auricular</td>
+                <td>Xiaomi</td>
+                <td>Redmi Airdots</td>
+                <td>60.000 $</td>
+            </tr>
+            <tr>
+                <td>Celular</td>
+                <td>Xiaomi</td>
+                <td>Redmi Note 8 Pro</td>
+                <td>700.000 $</td>
+            </tr>
+            <tr>
+                <td>Computador</td>
+                <td>Asus</td>
+                <td>TUF GAMING A11</td>
+                <td>3.500.000 $</td>
+            </tr>
+        </tbody>
+    </table>
+`
+
+export default html;
---
+++ generarPDF.js
@@ -0,0 +1,22 @@
+import html from './facturaHTML.js'; // <-- Aquí puedes elegir cualquier elemento del DOM
+html2pdf()
+    .set({
+        margin: 1,
+        filename: 'documento.pdf',
+        image: {
+            type: 'jpeg',
+            quality: 0.98
+        },
+        html2canvas: {
+            scale: 3, // A mayor escala, mejores gráficos, pero más peso
+            letterRendering: true,
+        },
+        jsPDF: {
+            unit: "in",
+            format: "a3",
+            orientation: 'landscape' // landscape o portrait
+        }
+    })
+    .from(html)
+    .save()
+    .catch(err => console.log(err));
